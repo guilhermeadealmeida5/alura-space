f96a99380c7f00a61d82210c4f180a296c9716d9
guilhermeadealmeida5 <guilhermeadealmeida5@gmail.com>
2024-04-08 22:45:54 -0300

    alteracoes

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from galeria.views import index
+from galeria.views import index, imagem
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
+    path('imagem', imagem, name='imagem'),
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'index.html')
-# Create your views here.
+    return render(request, 'galeria/index.html')
+
+def imagem(request):
+    return render(request, 'galeria/imagem.html')
