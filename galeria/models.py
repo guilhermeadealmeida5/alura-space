7fc249a5879e1ff282a22ca066b66f80355c4e40
guilhermeadealmeida5 <guilhermeadealmeida5@gmail.com>
2024-04-12 23:03:57 -0300

    ...

--- galeria/models.py
+++ galeria/models.py
@@ -1,10 +1,22 @@
 from django.db import models
+from datetime import datetime
 
 class Fotografia(models.Model):
+
+    OPCOES_CATEGORIA = [
+        ("NEBULOSA", "Nebulosa"),
+        ("ESTRELA", "Estrela"),
+        ("GALAXIA", "Galáxia"),
+        ("PLANETA", "Planeta")
+    ]
+
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
+    publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateField(default=datetime.now, blank=True)
 
     def __str__(self):
         return f"Fotografia [nome={self.nome}]"
--- galeria/views.py
+++ galeria/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografia
 
 def index(request):
-    fotografias = Fotografia.objects.all()	
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)	
 
     return render(request, 'galeria/index.html', {'cards': fotografias})
 
